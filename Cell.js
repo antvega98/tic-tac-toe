07f3284944ba6d73f7bff5bbec97998172cc72a8
antvega <antvega627@gmail.com>
2024-01-08 21:25:34 -0800

    Used svg for x and o labels.

--- Cell.js
+++ Cell.js
@@ -1,4 +1,29 @@
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Svg, { Circle, Line } from "react-native-svg";
+
+function X() {
+  return (
+    <Svg height={80} width={80} viewBox="0 0 100 100">
+      <Line x1={20} y1={20} x2={80} y2={80} stroke="black" strokeWidth={8} />
+      <Line x1={80} y1={20} x2={20} y2={80} stroke="black" strokeWidth={8} />
+    </Svg>
+  );
+}
+
+function O() {
+  return (
+    <Svg height={80} width={80}>
+      <Circle
+        cx={40}
+        cy={40}
+        r={30}
+        fill={"transparent"}
+        stroke={"black"}
+        strokeWidth={8}
+      />
+    </Svg>
+  );
+}
 
 export default function Cell({ dispatch, rowIndex, columnIndex, cellValue }) {
   const Container = cellValue === null ? TouchableOpacity : View;
@@ -13,7 +38,7 @@ export default function Cell({ dispatch, rowIndex, columnIndex, cellValue }) {
       : undefined;
   return (
     <Container onPress={onPress} style={styles.container}>
-      <Text style={styles.text}>{cellValue}</Text>
+      {cellValue === "X" ? <X /> : cellValue === "O" ? <O /> : null}
     </Container>
   );
 }
